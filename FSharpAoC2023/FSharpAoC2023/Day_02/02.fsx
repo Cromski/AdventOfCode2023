e71154c0481b574d0f9872cbe0f8b8a9994fc621
Cromski <jakobrhenriksen@outlook.com>
2023-12-01 10:36:03 +0100

    rdy for day 2

---
+++ FSharpAoC2023/FSharpAoC2023/Day_02/02.fsx
@@ -0,0 +1,6 @@
+let readLines filePath = List.ofSeq (System.IO.File.ReadLines(filePath))
+
+let ptOne (arr: string list) =
+    failwith "not implemeneted"
+
+printfn "pt 1: %A" (ptOne (readLines "./Day_02/mini.txt"))
