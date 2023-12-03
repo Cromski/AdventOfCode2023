6f17c027a30b8ddc32ec7be75da367bab16e98d7
Cromski <jakobrhenriksen@outlook.com>
2023-12-03 20:29:33 +0100

    solved day3

--- FSharpAoC2023/FSharpAoC2023/Day_03/03.fsx
+++ FSharpAoC2023/FSharpAoC2023/Day_03/03.fsx
@@ -27,22 +27,23 @@ let returnNumberIfAdjacentSymbol ((x,y):Coord) (matrix: string list) =
     then string (matrix[y][x])
     else "."
     
-let getFullNumberInStringFromCoord (str: string) ((x,y):Coord) (matrix: string list) =
+let getFullNumberInStringFromCoord ((x,y):Coord) (matrix: string list) =
+    if x < 0 || y < 0 || y >= matrix.Length || x >= matrix[y].Length then "" else
     if not (Char.IsDigit (matrix[y][x])) then "" else
     let rec aux1 acc x (str: string) =
         match x with
         | x when str = "" || x < 0 || not (Char.IsDigit str[x]) -> acc
         | x when Char.IsDigit (str[x]) -> aux1 (string (str[x])+acc) (x-1) str
-        | _ -> failwith "unforeseen"
+        | _ -> failwith "never"
         
     let rec aux2 acc x (str: string) =
         match x with
         | x when str = "" || x >= str.Length || not (Char.IsDigit str[x]) -> acc
         | x when Char.IsDigit (str[x]) -> aux2 (acc+string (str[x])) (x+1) str
-        | _ -> failwith "unforeseen"
-    aux1 "" (str.Substring(0,x).Length-1) (str.Substring(0,x))
+        | _ -> failwith "never"
+    aux1 "" (matrix[y].Substring(0,x).Length-1) (matrix[y].Substring(0,x))
     +
-    aux2 "" 0 (str.Substring(x,str.Length-x))
+    aux2 "" 0 (matrix[y].Substring(x,matrix[y].Length-x))
 
 let rec findNextNonDigit (str: string) (x: int) =
     let sub = str.Substring(x,str.Length-x)
@@ -59,7 +60,7 @@ let getIntListOfString (str: string) (row: int) (matrix: string list) =
         match s with
         | s when x >= s.Length -> acc
         | s when Char.IsDigit s[x] = false -> aux acc s (x+1)
-        | s when returnNumberIfAdjacentSymbol (x,row) matrix <> "." -> aux (acc@[getFullNumberInStringFromCoord s (x,row) matrix]) s (x+(findNextNonDigit s x))
+        | s when returnNumberIfAdjacentSymbol (x,row) matrix <> "." -> aux (acc@[getFullNumberInStringFromCoord (x,row) matrix]) s (x+(findNextNonDigit s x))
         | _ -> aux acc s (x+1)
     (aux [] str 0) |> List.map int |> List.sum
     
@@ -70,4 +71,53 @@ let ptOne (arr: string list) =
         | x :: xs -> aux (acc+(getIntListOfString x row arr)) (row+1) xs
     aux 0 0 arr
 
+let findStarsInString (str: string) (row: int) =
+    let rec aux acc sub (add: int) =
+        match sub with
+        | sub when sub = "" || sub.IndexOf("*") < 0 -> acc
+        | sub -> aux (acc@[(sub.IndexOf("*")+add,row)]) (sub.Substring(sub.IndexOf("*")+1)) (add+sub.IndexOf("*")+1)
+    aux [] str 0
+
+let getNumbersAroundStar ((x,y):Coord) (matrix: string list) =
+    [
+     getFullNumberInStringFromCoord (x-1,y-1) matrix; getFullNumberInStringFromCoord (x,y-1) matrix
+     getFullNumberInStringFromCoord (x+1,y-1) matrix; getFullNumberInStringFromCoord (x-1,y) matrix
+     getFullNumberInStringFromCoord (x+1,y) matrix; getFullNumberInStringFromCoord (x-1,y+1) matrix
+     getFullNumberInStringFromCoord (x,y+1) matrix; getFullNumberInStringFromCoord (x+1,y+1) matrix
+     ]
+
+let getNumbersAboutLotsOfStars (matrix: string list) ((x): Coord list) =
+    List.fold (fun acc (x,y) -> (getNumbersAroundStar (x,y) matrix)::acc) [] x
+
+let fixList (lst: string list) =
+    let rec aux acc (lst: string list) =
+        match lst with
+        | [] -> acc
+        | x :: y :: xs when x <> y -> aux (acc@[x;""]) (y::xs)
+        | _ :: y :: xs -> aux acc (y::xs)
+        | x :: xs -> aux (x::acc) xs
+    (aux [] lst) |> List.filter (fun s -> s <> "")
+    
+let fixLotsOfLists (lst: string list list) = 
+    List.fold (fun acc l -> fixList l::acc) [] lst
+
+let getGearPower (lst: string list) =
+    if lst.Length <> 2 then 0 else
+    lst
+    |> List.map int
+    |> List.fold (fun acc s -> s*acc) 1
+
+let ptTwo (arr: string list) =
+    let rec aux acc l (row: int) =
+        match l with
+        | [] -> acc
+        | x :: xs -> aux (acc@[findStarsInString x row]) xs (row+1)
+    (aux [] arr 0)
+    |> List.filter (fun l -> l <> [])
+    |> List.collect id
+    |> getNumbersAboutLotsOfStars arr
+    |> fixLotsOfLists
+    |> List.fold (fun acc a -> getGearPower a+acc) 0
+
 printfn "pt 1: %A" (ptOne (readLines "./Day_03/big.txt"))
+printfn "pt 2: %A" (ptTwo (readLines "./Day_03/big.txt"))
